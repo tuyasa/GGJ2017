171e6b740007bda58c2cc8ad53457d68216bda1a
tuyasa <tuyasa@live.fr>
2017-01-21 09:31:08 +0100

    merge

---
+++ ggj2017/Assets/Scripts/DrawLine.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour {
+
+	public SpriteRenderer sprite;
+	// Use this for initialization
+	void Start () {
+		sprite =  GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ggj2017/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speedX = 3f;
+	public float speedY = 3f;
+	 
+	LineRenderer line;
+	int pos;
+	public Vector2 currentPoint;
+
+	public int directionY;
+	// Use this for initialization
+	void Start () {
+//		directionY = 1;
+		currentPoint = Vector2.zero;
+		pos = 1;
+		line = GetComponent<LineRenderer>()	;
+	}	
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("dirY"+directionY);
+		currentPoint.x += speedX;
+		currentPoint.y += speedY * directionY;
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			directionY = directionY == 1 ? -1 : 1;
+			line.numPositions++;
+			pos++;
+			line.SetPosition(pos, currentPoint);
+		}
+		line.SetPosition(pos, currentPoint);
+	}
+}
