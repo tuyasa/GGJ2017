953de8be2a93aa59dcaf21cb26dee9adcadb1ad9
Orimaro <orimaro06@gmail.com>
2017-01-21 07:05:25 +0100

    Debut gameplay + trace Destroy player au contact d'un obstacle

---
+++ ggj2017/Assets/Scripts/DestroyCollide.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCollide : MonoBehaviour {
+
+	// Use this for initialization
+	void OnCollisionEnter ( Collision col)
+    {
+        if(col.gameObject.name == "Player")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
