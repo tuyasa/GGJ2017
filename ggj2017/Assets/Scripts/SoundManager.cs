34a27d1b2f1ed8b70d740c013da0d18ad002e5b7
Orimaro <orimaro06@gmail.com>
2017-01-21 02:13:08 +0100

    SoundManager

--- ggj2017/Assets/Scripts/SoundManager.cs
+++ ggj2017/Assets/Scripts/SoundManager.cs
@@ -4,16 +4,35 @@ using System.Collections.Generic;
 
 public class SoundManager : PersistentSingleton<SoundManager> {
 
-
+    public static SoundManager instance = null;
 	public AudioSource audio;
+
 	[System.Serializable]
 	public class LevelSounds {
-		public AudioClip bg;
-		public AudioClip win;
-		public AudioClip lose;
-	}
+		public AudioClip collectible;
+		public AudioClip start;
+        public AudioClip end;
+        public AudioClip background;
+
+    }
 
 	public List<LevelSounds> levelsSounds;
+
+    public void PlaySingle(AudioClip clip )
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
 	// Use this for initialization
 	void Start () {
 		audio = GetComponent<AudioSource>();
