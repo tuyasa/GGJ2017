d76115609f555047cb0c810151147608a8452485
tuyasa <tuyasa@live.fr>
2017-01-21 01:34:12 +0100

    managers

---
+++ ggj2017/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SoundManager : PersistentSingleton<SoundManager> {
+
+
+	public AudioSource audio;
+	[System.Serializable]
+	public class LevelSounds {
+		public AudioClip bg;
+		public AudioClip win;
+		public AudioClip lose;
+	}
+
+	public List<LevelSounds> levelsSounds;
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
